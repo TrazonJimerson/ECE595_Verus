0ecf955aeb9df2d7581bf33b5a3de99db480fb2e
Alex <aatcitty@unm.edu>
2026-04-02 12:52:54 -0600

    Removed Custom Environment files + hello world

--- Environment/_get_obs.py
+++
@@ -1,7 +0,0 @@
-def _get_obs(self):
-    """Convert internal state to observation format.
-
-    Returns:
-        dict: Observation with agent and target positions
-    """
-    return {"agent": self._agent_location, "target": self._target_location}
